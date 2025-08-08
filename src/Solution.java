defda241fc6af69078aa5f2ce01ec2a18419e6a9
chanakya <chanakya@chanakya>
2025-08-08 20:00:21 +0530

    solution

---
+++ src/Solution.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Solution {
+	static Scanner sc=new Scanner(System.in);
+	public static int Magiclibrary1() {
+		int row=sc.nextInt();
+		int col=sc.nextInt();
+		int[][] mat=new int[row][col];
+		int res=0;
+		for(int i=0;i<row;i++) {
+			int sum=0;
+			for(int j=0;j<col;j++) {
+				mat[i][j]=sc.nextInt();
+				if(mat[i][j]%2!=0) {
+					sum+=mat[i][j];
+				}
+			}
+			if(sum%2==0) {
+				res++;
+			}
+		}
+		sc.close();
+		return res;
+	}
+	
+	public static int knowledgeEnhancement3() {
+        System.out.println("Enter size of array:");
+        int size = sc.nextInt();
+        
+        int[] arr = new int[size];
+        System.out.println("Enter reading times of books:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter total available reading hours:");
+        int N = sc.nextInt();
+		Arrays.sort(arr);
+		int sum=0,count=0;
+		for(int num:arr) {
+			sum=sum+num;
+			if(sum<N) {
+				count++;
+			}
+			else {
+				break;
+			}
+		}
+		
+		return count;
+	}
+	
+	//4th
+	public static void distance4() {
+		String s=sc.next();
+		int i=0,j=s.length()-1;
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
